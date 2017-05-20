6090c9d2db15600bd18943656808952934e6dfbb
Xinbat007 <thienpq88@gmail.com>
2017-05-20 12:10:42 +0700

    1st Part 3

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+	public Sprite[] cardFace;
+	public Sprite cardBack;
+	public GameObject[] cards;
+	public Text matchText;
+
+	private bool _init = false;
+	private int _matches = 13;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_init) {
+			InitializeCards ();
+		}
+		if (Input.GetMouseButtonUp (0))
+			checkCards ();
+	}
+
+	void InitializeCards ()
+	{
+		for (int id = 0; id < 2; id++) {
+			for (int i = 1; i < 14; i++) {
+				bool test = false;
+				int choice = 0;
+				while (!test) {
+					choice = Random.Range (0, cards.Length);
+					test = !(cards [choice].GetComponent<Card> ().initialized);
+				}
+				cards [choice].GetComponent<Card> ().cardValue = i;
+				cards [choice].GetComponent<Card> ().initialized = true;
+			}
+		}
+		foreach (GameObject c in cards) {
+			c.GetComponent<Card> ().setupGraphics ();
+		}
+		if (!_init)
+			_init = true;
+	}
+
+	public Sprite getCardBack() {
+		return cardBack;
+	}
+
+	public Sprite getCardFace(int i) {
+		return cardFace [i - 1];
+	}
+
+	void checkCards() {
+		List<int> c = new List<int> ();
+	}
+}
---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour {
+
+	public void triggerMenuBehavior(int i) {
+		switch (i) {
+		case 0:
+			SceneManager.LoadScene ("Menu");
+			break;
+		case 1:
+			Application.Quit ();
+			break;
+		default:
+			break;
+		}
+	}
+}
